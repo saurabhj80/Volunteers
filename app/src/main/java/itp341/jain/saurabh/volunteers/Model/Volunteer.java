d506df62a55b8a22f0acb0148ddbbc86817091b2
Saurabh Jain <saurabhj80@Saurabhs-MacBook-Pro-2.local>
2016-11-25 23:00:59 -0800

    Added more info in model class

--- app/src/main/java/itp341/jain/saurabh/volunteers/Model/Volunteer.java
+++ app/src/main/java/itp341/jain/saurabh/volunteers/Model/Volunteer.java
@@ -7,15 +7,20 @@ import java.io.Serializable;
  */
 public class Volunteer implements Serializable
 {
-    // Data
+    // Basic Information
     private String title;
-    private String date;
     private String organization;
     private String description;
+
+    // Time and date
+    private String timeStart;
+    private String timeEnd;
+    private String date;
 
     // Location
     private double latitude;
     private double longitude;
+    private Address address;
 
     // Necessary for Firebase
     public Volunteer() {}
@@ -74,5 +79,73 @@ public class Volunteer implements Serializable
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+    public String getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(String timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public String getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(String timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public static class Address implements Serializable {
+
+        private String street;
+        private String city;
+        private String state;
+        private int zip;
+
+        public Address() {}
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public int getZip() {
+            return zip;
+        }
+
+        public void setZip(int zip) {
+            this.zip = zip;
+        }
     }
 }
